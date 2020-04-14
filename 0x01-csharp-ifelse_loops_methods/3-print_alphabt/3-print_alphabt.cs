f23c9d91331fd52e384ba8d48799cb486c7f372b
dilanrinconhbtn <937@holbertonschool.com>
2020-04-14 09:40:47 -0500

    print alphabet without e and q

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 97; i < 123; i++)
+            {
+                if (i != 101 && i != 113)
+                {
+                    Console.Write((char)i);                
+                }
+            }
+        }
+    }
+}
