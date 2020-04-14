f15e03cb3cd3e81ccd36be956431b75c53084639
dilanrinconhbtn <937@holbertonschool.com>
2020-04-14 13:22:36 -0500

    print line

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        if (length < 1)
+        {
+            Console.Write("\n");
+        }
+        else
+        {
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("_");
+            }
+        }
+    }
+}
