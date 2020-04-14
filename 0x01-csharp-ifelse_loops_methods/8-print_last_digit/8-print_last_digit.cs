30063cfd268161a43459dcfacaa4142171d1adfa
dilanrinconhbtn <937@holbertonschool.com>
2020-04-14 12:56:29 -0500

    print last digit in a class

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        if(number < 0)
+        {
+            number = number * -1;
+        }
+        int last = number % 10;
+        Console.Write("{0}", last);
+        return last;
+    }
+}
+
