1af2b00aeebc6738497dd3eb83fe2aafe32dccc8
dilanrinconhbtn <937@holbertonschool.com>
2020-04-16 16:06:37 -0500

    max of a list

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List {
+    public static int MaxInteger (List<int> myList) {
+        if (myList.Count > 0)
+        {
+            int max = myList[0];
+            for (int i = 0; i < myList.Count; i++)
+            {
+                if(max < myList[i])
+                {
+                    max = myList[i];
+                }
+            }
+            return max;
+        }
+        else
+        {
+            Console.WriteLine("List is empty");
+            return -1;
+        }
+    }
+}
