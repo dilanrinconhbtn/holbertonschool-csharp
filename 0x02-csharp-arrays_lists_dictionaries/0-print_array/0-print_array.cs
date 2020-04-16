c6e65845508fdf5ffd417b0e428112afa60e0e91
dilanrinconhbtn <937@holbertonschool.com>
2020-04-16 15:50:07 -0500

    creating lists

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -3,16 +3,16 @@
 class Array {
     public static int[] CreatePrint (int size) {
         if (size < 0) {
-            Console.WriteLine ("Size cannot be negative");
+            Console.WriteLine("Size cannot be negative");
             return null;
         } else {
             int[] numbers = new int[size];
             for (int i = 0; i < size; i++) {
                 numbers[i] = i;
                 if (i != size - 1) {
-                    Console.Write ("{0} ", numbers[i]);
+                    Console.Write("{0} ", numbers[i]);
                 } else {
-                    Console.Write ("{0}\n", numbers[i]);
+                    Console.Write("{0}\n", numbers[i]);
                 }
             }
             return numbers;
---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List {
+    public static List<int> CreatePrint (int size) {
+        List<int> Nlist = new List<int>();
+        if(size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        else if(size == 0)
+        {
+            Console.WriteLine();
+	        return Nlist;
+        }
+        else
+        {
+            for(int i=0; i < size;i++) 
+            {
+                if(i != size-1)
+                    Console.Write("{0} ",i);
+                else
+                    Console.Write(i);
+                Nlist.Add(i); 
+            }
+            Console.WriteLine();
+            return Nlist;
+        }
+    }
+}
