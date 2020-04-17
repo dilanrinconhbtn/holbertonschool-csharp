8acf82616520ac436054690dca743bd4fb29a94d
dilanrinconhbtn <937@holbertonschool.com>
2020-04-17 09:24:51 -0500

    keys

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary {
+    public static int NumberOfKeys(Dictionary<string, string> myDict) {
+        int i = 0;
+        foreach (KeyValuePair<string, string> k in myDict) {
+            i++;
+        }
+        return i;
+    }
+}
