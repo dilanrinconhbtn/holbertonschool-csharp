6e4424a81257cf74461787cdff6a1844e6272d6d
dilanrinconhbtn <937@holbertonschool.com>
2020-04-16 21:57:00 -0500

    delete fix

--- 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -2,15 +2,12 @@
 using System.Collections.Generic;
 
 class List {
-    public static List<int> DeleteAt(List<int> myList, int index) {
-        if (index > myList.Count || index < 0)
-        {
-            Console.WriteLine("Index is out of range");
+    public static List<int> DeleteAt (List<int> myList, int index) {
+        if (index > myList.Count || index < 0) {
+            Console.WriteLine ("Index is out of range");
             return myList;
-        }
-        else
-        {
-            myList.Remove(myList[index]);
+        } else {
+            myList.Remove (myList[index]);
             return myList;
         }
     }
