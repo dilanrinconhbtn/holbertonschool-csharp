01b1d74c03260f2b4b4e65d88e9d45a8a1482910
dilanrinconhbtn <937@holbertonschool.com>
2020-04-27 18:53:33 -0500

    full stack

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        int len = aStack.Count;
+        Console.WriteLine("Number of items: {0}", len);
+
+        if(len <= 0)
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        else
+        {
+            string top = aStack.Peek();
+            Console.WriteLine("Top item: {0}", top);
+        }
+        if (aStack.Contains(search))
+        {
+            Console.WriteLine("Stack contains \"{0}\": True", search);
+            while (aStack.Contains(search))
+            {
+                aStack.Pop();
+            }
+        }
+        else
+        {
+            Console.WriteLine("Stack contains \"{0}\": False", search);
+        }
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
+
