9ede323061aeac2936b03c9fe7dfb32e58f7218b
dilanrinconhbtn <937@holbertonschool.com>
2020-04-28 13:29:06 -0500

    safe list

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        try
+        {
+            int i;
+            for(i = 0; i < n; i++)
+                Console.WriteLine(myList[i]);
+            return i;
+        }
+        catch
+        {
+            return myList.Count;
+        }
+    }
+}
