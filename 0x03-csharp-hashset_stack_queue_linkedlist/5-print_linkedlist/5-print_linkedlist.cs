8cd2e659aa629f7cb57bfb399b88db94989329bb
dilanrinconhbtn <937@holbertonschool.com>
2020-04-27 20:23:11 -0500

    linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            LinkedList<int> empty = new LinkedList<int>();
+            return empty;
+        }
+        else
+        {
+            int[] num = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                num[i] = i;
+                Console.WriteLine(num[i]);
+            }
+            LinkedList<int> list = new LinkedList<int>(num);        
+            return list;
+        }
+    }
+}
+
