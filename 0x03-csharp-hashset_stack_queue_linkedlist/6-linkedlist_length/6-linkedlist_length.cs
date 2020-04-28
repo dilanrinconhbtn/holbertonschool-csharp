f7e70fa46dc79f2609f7d608a8d971852e1832e8
dilanrinconhbtn <937@holbertonschool.com>
2020-04-27 20:38:19 -0500

    length linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class LList
+{
+    public static int Length(LinkedList<int> myList)
+    {
+        int counter = 0;
+        foreach (int i in myList)
+        {
+            counter++;
+        }
+        return counter;
+    }
+}
+
