e37a688c1075c0697e7839dfb016a2e662966e73
dilanrinconhbtn <937@holbertonschool.com>
2020-04-28 21:26:14 -0500

    tostring

---
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+enum Rating
+{
+    Good, 
+    Great, 
+    Excellent
+}
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog(string NAME, float AGE, string OWNER, Rating RATING)
+    {
+        name = NAME;
+        age = AGE;
+        owner = OWNER;
+        rating = RATING;
+    }
+
+    public override string ToString()
+    {
+        return string.Format("Dog Name: {0}\nAge: {1}\nOwner: {2}\nRating: {3}", name, age, owner, rating);
+    }
+}
