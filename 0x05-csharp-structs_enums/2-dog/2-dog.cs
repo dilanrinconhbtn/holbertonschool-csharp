3eafc44d9eaa8005ab7f777d8087623c21434412
dilanrinconhbtn <937@holbertonschool.com>
2020-04-28 21:19:45 -0500

    construct inside struct

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+enum Rating
+{
+    Good, 
+    Great, 
+    Excellent
+}
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog(string NAME, float AGE, string OWNER, Rating RATING)
+    {
+        name = NAME;
+        age = AGE;
+        owner = OWNER;
+        rating = RATING;
+    }
+}
