66d0a0663e718fdd96c572fe4e4841cbd60381c7
dilanrinconhbtn <937@holbertonschool.com>
2020-04-30 20:49:33 -0500

    field health

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// This class Zombie does something.
+    /// </summary>
+    class Zombie
+    {
+        /// <summary> 
+        ///public field health = 0 
+        ///</summary>
+        public int health;
+        /// <summary> 
+        ///public class Zombie 
+        ///</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
