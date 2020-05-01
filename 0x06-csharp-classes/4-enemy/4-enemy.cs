52c4221bea12ec39cae883588dc6a87abe152843
dilanrinconhbtn <937@holbertonschool.com>
2020-04-30 22:51:52 -0500

    4 task

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// This class Zombie does something.
+    /// </summary>
+    class Zombie
+    {
+        /// <summary> 
+        ///public field health = 0 
+        ///</summary>
+        private int health;
+        private string name = "(No name)";
+        /// <summary> 
+        ///private class Zombie 
+        ///</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        public Zombie(int value)
+        {
+            if(value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                health = value;
+            }
+        }
+        public int GetHealth()
+        {
+            return health;
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name=value;
+            }
+        }
+    }
+}
