e5b14934752e8ac98c16bf196d854355d68b89ff
dilanrinconhbtn <937@holbertonschool.com>
2020-04-30 21:10:33 -0500

    givin a value to health

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// This class Zombie does something.
+    /// </summary>
+    class Zombie
+    {
+        /// <summary> 
+        ///public field health = 0 
+        ///</summary>
+        public int health;
+        /// <summary> 
+        ///public class Zombie 
+        ///</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        public Zombie(int value)
+        {
+            if(value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                health = value;
+            }
+        }
+    }
+}
