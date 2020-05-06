79f09c15024bec166a3c2433706b3e25c5accc2b
dilanrinconhbtn <937@holbertonschool.com>
2020-05-05 22:52:56 -0500

    My math file

---
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyMath
+{
+    public class Operations
+    {
+        public static int Max(List<int> nums)
+        {
+            if(nums == null)
+            {
+                return 0;
+            }
+            else
+            {
+                int max = nums[0];
+                foreach(int i in nums)
+                if (i > max)
+                    max = i;
+                return max;
+            }
+        }
+    }
+}
