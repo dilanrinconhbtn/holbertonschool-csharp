406d25b389d27d37b5a7ebdaf8b9a3c22fd19380
dilanrinconhbtn <937@holbertonschool.com>
2020-05-05 22:56:47 -0500

    adding comments and fix condition

--- 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MyMath
 {
+    ///<summary>Return the max integer in a list</summary>
     public class Operations
     {
+        /// <summary>Max integer</summary>
+        /// <param name="nums">list</param>
+        /// <returns>max integer in list, If nums is empty, return 0</returns>
         public static int Max(List<int> nums)
         {
             if(nums == null || nums <= 0)
