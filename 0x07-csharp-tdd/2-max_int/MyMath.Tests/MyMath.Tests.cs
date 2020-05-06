83ef290f182f710c26e5962c825a8139f6b98175
dilanrinconhbtn <937@holbertonschool.com>
2020-05-05 22:53:18 -0500

    My math tests

---
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,43 @@
+using NUnit.Framework;
+
+namespace MyMath.Tests
+{
+    public class OperationsTests
+    {
+        [Test]
+        public void PositiveResult()
+        {
+            List<int> digits = new List<int> { 1, 2, 3, 4, 8, 2, 92, 102, 304, 705, 507, -803 }; 
+
+            int result = Operations.Max(digits);
+
+            Assert.AreEqual(705, result);
+        }
+        [Test]
+        public void NegativeResult()
+        {
+            List<int> num = new List<int> { -803, -205, -301, -5 }; 
+
+            int result = Operations.Max(num);
+
+            Assert.AreEqual(-5, result);
+        }
+        [Test]
+        public void ListEmpty()
+        {
+            List<int> num = new List<int>(); 
+
+            int result = Operations.Max(num);
+
+            Assert.AreEqual(0, result);
+        }
+        public void ListNull()
+        {
+            List<int> num = new List<int>(); 
+
+            int result = Operations.Max(num);
+
+            Assert.AreEqual(0, result);
+        }
+    }
+}
