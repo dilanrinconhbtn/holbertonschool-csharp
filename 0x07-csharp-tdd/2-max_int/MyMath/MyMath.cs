f9382cd982800b6991ee4276f7adc66b500e6ebd
dilanrinconhbtn <937@holbertonschool.com>
2020-05-05 23:22:59 -0500

    delete else

--- 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -11,18 +11,15 @@ namespace MyMath
         /// <returns>max integer in list, If nums is empty, return 0</returns>
         public static int Max(List<int> nums)
         {
-            if(nums == null || nums.Count <= 0)
+            if (nums == null || nums.Count <= 0)
             {
                 return 0;
             }
-            else
-            {
-                int max = nums[0];
-                foreach(int i in nums)
+            int max = nums[0];
+            foreach (int i in nums)
                 if (i > max)
                     max = i;
-                return max;
-            }
+            return max;
         }
     }
 }
