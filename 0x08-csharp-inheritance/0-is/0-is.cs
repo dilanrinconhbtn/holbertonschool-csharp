66e4e1b6186d4b106372b427a88e0c7af70c1eca
dilanrinconhbtn <937@holbertonschool.com>
2020-05-07 16:22:02 -0500

    change position of comment

--- 0x08-csharp-inheritance/0-is/0-is.cs
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 
 class Obj
 {
+    ///<summary>returns True if the object is an int, otherwise return False</summary>
     public static bool IsOfTypeInt(object obj)
     {
-        //Console.WriteLine(Object.ReferenceEquals(obj.GetType(), obj.GetType()));
-        ///<summary>returns True if the object is an int, otherwise return False</summary>
+        
         if(obj.GetType() == typeof(int))
         {
             return true;
