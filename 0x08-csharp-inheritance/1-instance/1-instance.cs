92df89c7da066b45bc8914d29685125933c65699
dilanrinconhbtn <937@holbertonschool.com>
2020-05-07 10:45:57 -0500

    inherit true or false

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Obj
+{
+    public static bool IsInstanceOfArray(object obj)
+    {
+        //Console.WriteLine(Object.ReferenceEquals(obj.GetType(), obj.GetType()));
+
+        
+
+        if(typeof(Array).IsInstanceOfType(obj))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
