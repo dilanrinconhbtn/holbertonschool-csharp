0d2561165c8e226fca7b18f5966f9d1159932c91
dilanrinconhbtn <937@holbertonschool.com>
2020-05-07 17:42:59 -0500

    print types

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Globalization;
+using System.Text;
+
+
+class Obj
+{
+    ///<summary>print all properties of a object</summary>
+    public static void Print(object myObj)
+    {
+        TypeInfo t = myObj.GetType().GetTypeInfo();
+        IEnumerable<PropertyInfo> pList  = t.GetProperties();
+        IEnumerable<MethodInfo> mList = t.GetMethods();
+        System.Console.WriteLine("{0} Properties:", t.Name);
+        foreach (PropertyInfo p in pList)
+        {
+            System.Console.WriteLine(p.Name);
+        }
+        System.Console.WriteLine("{0} Methods:", t.Name);
+        foreach (MethodInfo m in mList)
+        {
+            System.Console.WriteLine(m.Name);
+        }
+    }
+}
+
