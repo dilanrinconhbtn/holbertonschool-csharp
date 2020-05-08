bdce3b2a1636d88806e23312bff113a9eb364565
dilanrinconhbtn <937@holbertonschool.com>
2020-05-07 19:52:43 -0500

    get and set

---
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+class Shape
+{
+    ///<summary>throw exception NotImplementedException</summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    ///<summary>Width</summary>
+    public int Width
+    {
+        get
+        {
+            return width;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            else
+            {
+                width = value;
+            }
+        }
+    }
+    //<summary>Height</summary>
+    public int Height
+    {
+        get
+        {
+            return height;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+            else
+            {
+                height = value;
+            }
+        }
+    }
+}
+
